6470ec42ff02c4c38baf010401ee689f04007fb0
Deborah <deborah_rimei@hotmail.com>
2020-12-06 19:11:22 +0000

    changed font, added image and updated recipes container

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -2,17 +2,22 @@ import React from 'react';
 import "./Recipe.css"
 
 
-const Recipe = ({ title, ingredients, dietLabels, image}) => {
+const Recipe = ({ title, ingredients, dietLabels, image }) => {
     return (
-        <div className="recipe-container">
+        <div className="recipe-wrapper">
             <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{dietLabels}</p>
-            <img src={image} alt={title}></img>
+            <div className="ingredients">
+                <div>
+                    <h3>Ingredients</h3>
+                    <ol>
+                        {ingredients.map(ingredient => (
+                            <li>{ingredient.text}</li>
+                        ))}
+                    </ol>
+                    <p>{dietLabels}</p>
+                </div>
+                <img src={image} alt={title}></img>
+            </div>
         </div>
     );
 }
---
+++ src/Components/ResultBar/ResultBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./ResultBar.css"
+
+
+const ResultBar = ({search}) => {
+    return (
+        <div className = "result">
+            <h3>Results for</h3>
+            <span>{search}</span>
+        </div>
+    );
+}
+
+
+
+export default ResultBar;
