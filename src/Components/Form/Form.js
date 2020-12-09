5af2b4623dfff2d562c60c72a15e81c65c36943e
Deborah <deborah_rimei@hotmail.com>
2020-12-09 18:25:42 +0000

    change responsiveness of the recipes

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,7 +4,7 @@ import "./Form.css"
 const Form = ({ getSearch, search, updateSearch }) => {
     return (
         <div className="form-container">
-            <h3>Look up recepies by ingredients, get inspired..</h3>
+            <h3>Look up recepies by ingredient, get inspired..</h3>
             <form onSubmit={getSearch} className="search-form">
                 <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="write your ingredient here.."></input>
                 <button className="search-button" type="submit">
